a81965fd5371f39cc6d5752bd6ec47665ab913d1
Andrew Witten <andrew.witten@mongodb.com>
2022-07-19 01:58:29 +0000

    new endpoints

--- app.js
+++ app.js
@@ -18,13 +18,40 @@ async function findOne(hostname, date) {
     return result.flamegraph;
 }
 
-app.get('/:hostname/:datetime', async function(req, res, next) {
+async function findDistinct() {
+    const res = await flamegraphs.distinct("hostname")
+    console.log(`Found hostnames: ${res}`)
+    return res
+}
+
+app.get('/:hostname/flamegraph/:datetime', async function(req, res, next) {
     res.setHeader('Content-Type', 'image/svg+xml');
     findOne(req.params.hostname, new Date(req.params.datetime))
         .then((fg) => res.send(fg))
         .catch(next);
 })
 
+app.get('/:hostname/timestamps', async function(req, res, next) {
+    const query = {hostname: req.params.hostname}
+    const options = {projection: {date: 1}}
+    const cursor = flamegraphs.find(query, options).limit(10)
+    const count = await cursor.count()
+    console.log(`Found ${count} docs in handling req ${req.originalUrl}`);
+    const timestamps = [];
+    try {
+        await cursor.forEach((doc) => { timestamps.push(doc); });
+    } catch (err) {
+        next(err)
+    }
+    res.setHeader('Content-Type', "application/json");
+    res.send({timestamps: timestamps})
+});
+
+app.get('/hostnames', async function(_, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    findDistinct().then((hostnames) => res.send({hostnames: hostnames})).catch(next)
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
