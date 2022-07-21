45cbd3941f4925712da2bb20afa08b903729b2a1
Kruti Shah <kruti139@gmail.com>
2022-07-21 17:41:32 +0000

    changed color theme

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import Hostnames from './Hostnames'
 import Timestamps from './Timestamps'
-import Timestamps2 from './Timestamps2'
+
 class App extends Component {
   render() {
     return (<div className = 'container'><BrowserRouter><Routes>
